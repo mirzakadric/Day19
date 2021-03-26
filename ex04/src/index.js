eabc825bee263001fee82a4d14edf204d8251f3e
Mirza Kadric <kadric.mirza@hotmail.com>
2021-03-26 19:56:25 +0100

    Adding folder ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,21 @@
+// src/index.js
+
+import React, {Component} from 'react' // Change this line
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// add the following code below this line
+
+class App extends Component {  // Change this line
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, Padawans!</h1>
+      </div>
+    )
+  }
+}
+
+// add the following code below this line
+
+ReactDOM.render(<App/>, document.getElementById('root'));
